9103a3b0e130f9d24fb936fa03bce6777dc82343
Daniella Otarola <daniellaotarola@daniellas-macbook-air.local>
2015-11-07 22:39:28 -0500

    Initial Commit

---
+++ Island Corps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Island Corps
+//
+//  Created by Daniella Otarola on 11/7/15.
+//  Copyright © 2015 Island Corps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
